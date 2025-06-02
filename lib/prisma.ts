f37b8d389740bae8cf1449e1f4ac010ac7e23405
zhangtao25 <wr.zhang25@gmail.com>
2025-06-02 22:45:48 +0800

    chore: daily development

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@/app/generated/prisma-client'
+// import { PrismaClient } from '@/app/generated/prisma-client'
+import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prisma = new PrismaClient().$extends(withAccelerate())
